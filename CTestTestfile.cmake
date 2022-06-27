a2bcee3cbe00ab724daa314793e4818d481b2707
pzr1988 <zherong@cs.unc.edu>
2022-06-27 15:49:03 -0500

    removed unnecessary files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yxhan/yxh/kernel-based-navigation-master
-# Build directory: /home/yxhan/yxh/kernel-based-navigation-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")

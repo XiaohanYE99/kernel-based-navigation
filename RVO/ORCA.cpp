be4a68bc22d6f0370ecae8e24bee6dd621accbc0
Zherong <zherong@cs.unc.edu>
2022-08-03 00:35:43 -0700

    most AD functions tested

--- RVO/ORCA.cpp
+++ RVO/ORCA.cpp
@@ -27,8 +27,8 @@ bool ORCASimulator::optimize(bool requireGrad,bool output) {
   if(output)
     std::cout << "Computing agent velocity obstacles!" << std::endl;
   auto computeVAFunc=[&](AgentNeighbor n)->bool{
-    VelocityObstacle VO0=computeVelocityObstacle(n._v[0]->_id,n._v[1]->_id,requireGrad);
-    VelocityObstacle VO1=computeVelocityObstacle(n._v[1]->_id,n._v[0]->_id,requireGrad);
+    VelocityObstacle VO0=computeVelocityObstacle(n._v[0]->_id,n._v[1]->_id);
+    VelocityObstacle VO1=computeVelocityObstacle(n._v[1]->_id,n._v[0]->_id);
     updateLP(VO0);
     updateLP(VO1);
     return true;
@@ -41,7 +41,7 @@ bool ORCASimulator::optimize(bool requireGrad,bool output) {
     std::cout << "Computing agent-obstacle velocity obstacles!" << std::endl;
   auto computeVOFunc=[&](AgentObstacleNeighbor n)->bool {
     Vec2T obs[2]={n._o->_pos,n._o->_next->_pos};
-    VelocityObstacle VO=computeVelocityObstacle(n._v->_id,obs,requireGrad);
+    VelocityObstacle VO=computeVelocityObstacle(n._v->_id,obs);
     updateLP(VO);
     return true;
   };
@@ -70,8 +70,10 @@ void ORCASimulator::debugVO(int aid,int bid,int testCase,T eps) {
     _perfVelocities.setRandom();
     if((_agentPositions.col(aid)-_agentPositions.col(bid)).norm()<=_rad*2)
       continue;
-    VelocityObstacle VO0=computeVelocityObstacle(aid,bid,true);
-    VelocityObstacle VO1=computeVelocityObstacle(bid,aid,true);
+    VelocityObstacle VO0=computeVelocityObstacle(aid,bid);
+    VelocityObstacle VO1=computeVelocityObstacle(bid,aid);
+    debugDerivatives(VO0);
+    debugDerivatives(VO1);
     ASSERT_MSGV(VO0._case==VO1._case,"%d=VO0._case!=VO1._case=%d!",VO0._case,VO1._case)
     if(VO0._case!=testCase)
       continue;
@@ -93,8 +95,8 @@ void ORCASimulator::debugVO(int aid,int bid,int testCase,T eps) {
     _perfVelocities.setRandom();
     if((_agentPositions.col(aid)-_agentPositions.col(bid)).norm()<=_rad*2)
       continue;
-    VelocityObstacle VO0=computeVelocityObstacle(aid,bid,true);
-    VelocityObstacle VO1=computeVelocityObstacle(bid,aid,true);
+    VelocityObstacle VO0=computeVelocityObstacle(aid,bid);
+    VelocityObstacle VO1=computeVelocityObstacle(bid,aid);
     ASSERT_MSGV(VO0._case==VO1._case,"%d=VO0._case!=VO1._case=%d!",VO0._case,VO1._case)
     if(VO0._case!=testCase)
       continue;
@@ -113,8 +115,8 @@ void ORCASimulator::debugVO(int aid,int bid,int testCase,T eps) {
     _perfVelocities.setRandom();
     if((_agentPositions.col(aid)-_agentPositions.col(bid)).norm()<=_rad*2)
       continue;
-    VelocityObstacle VO0=computeVelocityObstacle(aid,bid,true);
-    VelocityObstacle VO1=computeVelocityObstacle(bid,aid,true);
+    VelocityObstacle VO0=computeVelocityObstacle(aid,bid);
+    VelocityObstacle VO1=computeVelocityObstacle(bid,aid);
     ASSERT_MSGV(VO0._case==VO1._case,"%d=VO0._case!=VO1._case=%d!",VO0._case,VO1._case)
     if(VO0._case!=testCase)
       continue;
@@ -140,8 +142,8 @@ void ORCASimulator::debugVO(int aid,int testCase,bool mustInside,T eps) {
     Vec2T o[2]= {Vec2T::Random(),Vec2T::Random()};
     if(BoundingVolumeHierarchy::distance(_agentPositions.col(aid),o)<=_rad)
       continue;
-    Vec2T oCopy[2]= {o[0],o[1]};
-    VelocityObstacle VO=computeVelocityObstacle(aid,oCopy,true);
+    VelocityObstacle VO=computeVelocityObstacle(aid,o);
+    debugDerivatives(VO,o);
     if(VO._case!=testCase)
       continue;
     _perfVelocities.col(aid)=VO.proj(_perfVelocities.col(aid));
@@ -163,8 +165,7 @@ void ORCASimulator::debugVO(int aid,int testCase,bool mustInside,T eps) {
     Vec2T o[2]= {Vec2T::Random(),Vec2T::Random()};
     if(BoundingVolumeHierarchy::distance(_agentPositions.col(aid),o)<=_rad)
       continue;
-    Vec2T oCopy[2]= {o[0],o[1]};
-    VelocityObstacle VO=computeVelocityObstacle(aid,oCopy,true);
+    VelocityObstacle VO=computeVelocityObstacle(aid,o);
     if(VO._case!=testCase)
       continue;
     Vec2T edgeA[2]= {_agentPositions.col(aid),_agentPositions.col(aid)+_perfVelocities.col(aid)*_timestep};
@@ -181,8 +182,7 @@ void ORCASimulator::debugVO(int aid,int testCase,bool mustInside,T eps) {
     Vec2T o[2]= {Vec2T::Random(),Vec2T::Random()};
     if(BoundingVolumeHierarchy::distance(_agentPositions.col(aid),o)<=_rad)
       continue;
-    Vec2T oCopy[2]= {o[0],o[1]};
-    VelocityObstacle VO=computeVelocityObstacle(aid,oCopy,true);
+    VelocityObstacle VO=computeVelocityObstacle(aid,o);
     if(VO._case!=testCase)
       continue;
     Vec2T edgeA[2]= {_agentPositions.col(aid),_agentPositions.col(aid)+_perfVelocities.col(aid)*_timestep};
@@ -193,189 +193,266 @@ void ORCASimulator::debugVO(int aid,int testCase,bool mustInside,T eps) {
     break;
   }
 }
+void ORCASimulator::debugDerivatives(const VelocityObstacle& VO) {
+  Vec2T d=Vec2T::Random(),old;
+  VelocityObstacle VO2;
+  DEFINE_NUMERIC_DELTA_T(T)
+  //pa
+  old=_agentPositions.col(VO._aid);
+  _agentPositions.col(VO._aid)+=d*Delta;
+  VO2=computeVelocityObstacle(VO._aid,VO._bid);
+  DEBUG_GRADIENT("DposDpa",(VO.DposDpa()*d).norm(),(VO.DposDpa()*d-(VO2.pos()-VO.pos())/Delta).norm())
+  DEBUG_GRADIENT("DnorDpa",(VO.DnorDpa()*d).norm(),(VO.DnorDpa()*d-(VO2.nor()-VO.nor())/Delta).norm())
+  _agentPositions.col(VO._aid)=old;
+  //va
+  old=_perfVelocities.col(VO._aid);
+  _perfVelocities.col(VO._aid)+=d*Delta;
+  VO2=computeVelocityObstacle(VO._aid,VO._bid);
+  DEBUG_GRADIENT("DposDva",(VO.DposDva()*d).norm(),(VO.DposDva()*d-(VO2.pos()-VO.pos())/Delta).norm())
+  DEBUG_GRADIENT("DnorDva",(VO.DnorDva()*d).norm(),(VO.DnorDva()*d-(VO2.nor()-VO.nor())/Delta).norm())
+  _perfVelocities.col(VO._aid)=old;
+  //pb
+  old=_agentPositions.col(VO._bid);
+  _agentPositions.col(VO._bid)+=d*Delta;
+  VO2=computeVelocityObstacle(VO._aid,VO._bid);
+  DEBUG_GRADIENT("DposDpb",(VO.DposDpb()*d).norm(),(VO.DposDpb()*d-(VO2.pos()-VO.pos())/Delta).norm())
+  DEBUG_GRADIENT("DnorDpb",(VO.DnorDpb()*d).norm(),(VO.DnorDpb()*d-(VO2.nor()-VO.nor())/Delta).norm())
+  _agentPositions.col(VO._bid)=old;
+  //vb
+  old=_perfVelocities.col(VO._bid);
+  _perfVelocities.col(VO._bid)+=d*Delta;
+  VO2=computeVelocityObstacle(VO._aid,VO._bid);
+  DEBUG_GRADIENT("DposDvb",(VO.DposDvb()*d).norm(),(VO.DposDvb()*d-(VO2.pos()-VO.pos())/Delta).norm())
+  DEBUG_GRADIENT("DnorDvb",(VO.DnorDvb()*d).norm(),(VO.DnorDvb()*d-(VO2.nor()-VO.nor())/Delta).norm())
+  _perfVelocities.col(VO._bid)=old;
+}
+void ORCASimulator::debugDerivatives(const VelocityObstacle& VO,const Vec2T o[2]) {
+  Vec2T d=Vec2T::Random(),old;
+  VelocityObstacle VO2;
+  DEFINE_NUMERIC_DELTA_T(T)
+  //pa
+  old=_agentPositions.col(VO._aid);
+  _agentPositions.col(VO._aid)+=d*Delta;
+  VO2=computeVelocityObstacle(VO._aid,o);
+  DEBUG_GRADIENT("DposDpa",(VO.DposDpa()*d).norm(),(VO.DposDpa()*d-(VO2.pos()-VO.pos())/Delta).norm())
+  DEBUG_GRADIENT("DnorDpa",(VO.DnorDpa()*d).norm(),(VO.DnorDpa()*d-(VO2.nor()-VO.nor())/Delta).norm())
+  _agentPositions.col(VO._aid)=old;
+  //va
+  old=_perfVelocities.col(VO._aid);
+  _perfVelocities.col(VO._aid)+=d*Delta;
+  VO2=computeVelocityObstacle(VO._aid,o);
+  DEBUG_GRADIENT("DposDva",(VO.DposDva()*d).norm(),(VO.DposDva()*d-(VO2.pos()-VO.pos())/Delta).norm())
+  DEBUG_GRADIENT("DnorDva",(VO.DnorDva()*d).norm(),(VO.DnorDva()*d-(VO2.nor()-VO.nor())/Delta).norm())
+  _perfVelocities.col(VO._aid)=old;
+}
 //helper
-ORCASimulator::VelocityObstacle ORCASimulator::computeVelocityObstacle(int aid,int bid,bool requireGrad) const {
+ORCASimulator::VelocityObstacle ORCASimulator::computeVelocityObstacle(int aid,int bid) const {
   VelocityObstacle ret;
   ret._aid=aid;
   ret._bid=bid;
-  if(requireGrad) {
-    ret._DvnDpva.setZero();
-    ret._DvnDpvb.setZero();
-  }
-  Vec2T rab=_agentPositions.col(aid)-_agentPositions.col(bid),rabInvT=rab/_timestep;
-  Vec2T vba=_perfVelocities.col(bid)-_perfVelocities.col(aid);
-  T lenRab=rab.norm(),lenRabInvT=lenRab/_timestep;
+  Vec2TAD pa,pb,va,vb;
+  pa[0]=AD(_agentPositions(0,aid),Derivative::Unit(0));
+  pa[1]=AD(_agentPositions(1,aid),Derivative::Unit(1));
+  pb[0]=AD(_agentPositions(0,bid),Derivative::Unit(2));
+  pb[1]=AD(_agentPositions(1,bid),Derivative::Unit(3));
+  va[0]=AD(_perfVelocities(0,aid),Derivative::Unit(4));
+  va[1]=AD(_perfVelocities(1,aid),Derivative::Unit(5));
+  vb[0]=AD(_perfVelocities(0,bid),Derivative::Unit(6));
+  vb[1]=AD(_perfVelocities(1,bid),Derivative::Unit(7));
+  Vec2TAD rab=pa-pb,rabInvT=rab/_timestep,vba=vb-va;
+  AD lenRab=rab.norm(),lenRabInvT=lenRab/_timestep;
   //check whether vab resides in the tip of cone
-  T cosTheta=_rad*2/lenRab;
-  T distToTip=(vba-rabInvT).norm();
-  T cosThetaTip=(vba-rabInvT).dot(-rabInvT)/distToTip/lenRabInvT;
-  if(cosThetaTip>cosTheta) {
+  AD cosTheta=_rad*2/lenRab;
+  AD distToTip=(vba-rabInvT).norm();
+  AD cosThetaTip=(vba-rabInvT).dot(-rabInvT)/distToTip/lenRabInvT;
+  if(cosThetaTip.value()>cosTheta.value()) {
     //in the tip
-    Vec2T newVba=(vba-rabInvT)/distToTip*_rad*2/_timestep+rabInvT;
-    ret.pos()=_perfVelocities.col(ret._aid)-(newVba-vba)/2;
-    ret.nor()=-(vba-rabInvT)/distToTip;
+    Vec2TAD newVba=(vba-rabInvT)/distToTip*_rad*2/_timestep+rabInvT;
+    ret._pos=_perfVelocities.col(ret._aid)-(newVba-vba)/2;
+    ret._nor=-(vba-rabInvT)/distToTip;
     ret._case=0;
   } else if(cross2D(rab,vba)>0) {
     //left side
-    ret.nor()=rot2D(_rad*2,lenRab)*rab/lenRab;
-    Vec2T newVba=vba.dot(ret.nor())*ret.nor();
-    ret.pos()=_perfVelocities.col(ret._aid)-(newVba-vba)/2;
-    ret.nor()=Vec2T(ret.nor()[1],-ret.nor()[0]);
+    ret._nor=rot2D(_rad*2,lenRab)*rab/lenRab;
+    Vec2TAD newVba=vba.dot(ret._nor)*ret._nor;
+    ret._pos=_perfVelocities.col(ret._aid)-(newVba-vba)/2;
+    ret._nor=Vec2TAD(ret._nor[1],-ret._nor[0]);
     ret._case=1;
   } else {
     //right side
-    ret.nor()=rot2D(_rad*2,lenRab).transpose()*rab/lenRab;
-    Vec2T newVba=vba.dot(ret.nor())*ret.nor();
-    ret.pos()=_perfVelocities.col(ret._aid)-(newVba-vba)/2;
-    ret.nor()=Vec2T(-ret.nor()[1],ret.nor()[0]);
+    ret._nor=rot2D(_rad*2,lenRab).transpose()*rab/lenRab;
+    Vec2TAD newVba=vba.dot(ret._nor)*ret._nor;
+    ret._pos=_perfVelocities.col(ret._aid)-(newVba-vba)/2;
+    ret._nor=Vec2TAD(-ret._nor[1],ret._nor[0]);
     ret._case=2;
   }
   return ret;
 }
-ORCASimulator::VelocityObstacle ORCASimulator::computeVelocityObstacle(int aid,Vec2T o[2],bool requireGrad) const {
+ORCASimulator::VelocityObstacle ORCASimulator::computeVelocityObstacle(int aid,const Vec2T o[2]) const {
   VelocityObstacle ret;
   ret._aid=aid;
   ret._bid=-1;
-  if(requireGrad) {
-    ret._DvnDpva.setZero();
-    ret._DvnDpvb.setZero();
-  }
-  o[0]-=_agentPositions.col(aid);
-  o[1]-=_agentPositions.col(aid);
-  if(cross2D(o[0],o[1])>0)
-    std::swap(o[0],o[1]);
-  Vec2T nLL=rot2D(_rad,o[0].norm())*o[0];
-  Vec2T nLR=rot2D(_rad,o[0].norm()).transpose()*o[0];
-  Vec2T nRL=rot2D(_rad,o[1].norm())*o[1];
-  Vec2T nRR=rot2D(_rad,o[1].norm()).transpose()*o[1];
+  Vec2TAD pa,va,O[2];
+  pa[0]=AD(_agentPositions(0,aid),Derivative::Unit(0));
+  pa[1]=AD(_agentPositions(1,aid),Derivative::Unit(1));
+  va[0]=AD(_perfVelocities(0,aid),Derivative::Unit(4));
+  va[1]=AD(_perfVelocities(1,aid),Derivative::Unit(5));
+  for(int a=0; a<2; a++)
+    for(int b=0; b<2; b++)
+      O[a][b]=AD(o[a][b],Derivative::Zero());
+  O[0]-=pa;
+  O[1]-=pa;
+  if(cross2D(O[0],O[1])>0)
+    std::swap(O[0],O[1]);
+  Vec2TAD nLL=rot2D(_rad,O[0].norm())*O[0];
+  Vec2TAD nLR=rot2D(_rad,O[0].norm()).transpose()*O[0];
+  Vec2TAD nRL=rot2D(_rad,O[1].norm())*O[1];
+  Vec2TAD nRR=rot2D(_rad,O[1].norm()).transpose()*O[1];
+  Vec2TAD a[2]= {Vec2TAD::Zero(),va*_timestep};
   if(cross2D(nRL,nLL)>=0 && cross2D(nRR,nLR)<=0) {
     ret._case=0;
-    computeVelocityObstacle(ret,o[0],nLL,nLR,requireGrad);
+    computeVelocityObstacle(ret,O[0],a[1],nLL,nLR);
   } else if(cross2D(nLL,nRL)>=0 && cross2D(nLR,nRR)<=0) {
     ret._case=5;
-    computeVelocityObstacle(ret,o[1],nRL,nRR,requireGrad);
+    computeVelocityObstacle(ret,O[1],a[1],nRL,nRR);
   } else {
     ret._case=10;
-    computeVelocityObstacle(ret,o,nLL,nRR,requireGrad);
+    computeVelocityObstacle(ret,O,a,nLL,nRR);
   }
   return ret;
 }
-void ORCASimulator::computeVelocityObstacle(VelocityObstacle& ret,const Vec2T& o,const Vec2T& dL,const Vec2T& dR,bool requireGrad) const {
-  Vec2T nL=Vec2T(dL[1],-dL[0]).normalized();
-  Vec2T nR=Vec2T(-dR[1],dR[0]).normalized();
-  Vec2T a=_perfVelocities.col(ret._aid)*_timestep;
-  T distL=(a-o).dot(nL);
-  T distR=(a-o).dot(nR);
+void ORCASimulator::computeVelocityObstacle(VelocityObstacle& ret,const Vec2TAD& o,const Vec2TAD& a,const Vec2TAD& dL,const Vec2TAD& dR) const {
+  Vec2TAD nL=Vec2TAD(dL[1],-dL[0]).normalized();
+  Vec2TAD nR=Vec2TAD(-dR[1],dR[0]).normalized();
+  AD distL=(a-o).dot(nL);
+  AD distR=(a-o).dot(nR);
   if(distL>=0 && distR>=0) {
     //check 2 voronoi region
     if(distL<distR) {
       //closest feature is left border
-      ret.pos()=(a-nL*(distL+_rad))/_timestep;
-      ret.nor()=-nL;
+      ret._pos=(a-nL*(distL+_rad))/_timestep;
+      ret._nor=-nL;
       ret._case+=0;
     } else {
       //closest feature is right border
-      ret.pos()=(a-nR*(distR+_rad))/_timestep;
-      ret.nor()=-nR;
+      ret._pos=(a-nR*(distR+_rad))/_timestep;
+      ret._nor=-nR;
       ret._case+=1;
     }
   } else {
     //check 3 voronoi region
     if((a-o).dot(dL)>0) {
       //closest feature is left border
-      T distL=(a-o).dot(nL);
-      ret.pos()=(a-nL*(distL+_rad))/_timestep;
-      ret.nor()=-nL;
+      AD distL=(a-o).dot(nL);
+      ret._pos=(a-nL*(distL+_rad))/_timestep;
+      ret._nor=-nL;
       ret._case+=2;
     } else if((a-o).dot(dR)>0) {
       //closest feature is right border
-      T distR=(a-o).dot(nR);
-      ret.pos()=(a-nR*(distR+_rad))/_timestep;
-      ret.nor()=-nR;
+      AD distR=(a-o).dot(nR);
+      ret._pos=(a-nR*(distR+_rad))/_timestep;
+      ret._nor=-nR;
       ret._case+=3;
     } else {
       //closest feature is left vertex
-      T distVL=(a-o).norm();
-      ret.nor()=(a-o)/fmax(distVL,Epsilon<T>::defaultEps());
-      ret.pos()=(o+ret.nor()*_rad)/_timestep;
+      AD distVL=(a-o).norm();
+      if(distVL.value()>Epsilon<T>::defaultEps())
+        ret._nor=(a-o)/distVL;
+      else ret._nor=(a-o)/Epsilon<T>::defaultEps();
+      ret._pos=(o+ret._nor*_rad)/_timestep;
       ret._case+=4;
     }
   }
 }
-void ORCASimulator::computeVelocityObstacle(VelocityObstacle& ret,const Vec2T o[2],const Vec2T& dL,const Vec2T& dR,bool requireGrad) const {
-  Vec2T dirO=o[1]-o[0];
-  Vec2T nO=Vec2T(-dirO[1],dirO[0]).normalized();
-  Vec2T nL=Vec2T(dL[1],-dL[0]).normalized();
-  Vec2T nR=Vec2T(-dR[1],dR[0]).normalized();
+void ORCASimulator::computeVelocityObstacle(VelocityObstacle& ret,const Vec2TAD o[2],const Vec2TAD a[2],const Vec2TAD& dL,const Vec2TAD& dR) const {
+  Vec2TAD dirO=o[1]-o[0];
+  Vec2TAD nO=Vec2TAD(-dirO[1],dirO[0]).normalized();
+  Vec2TAD nL=Vec2TAD(dL[1],-dL[0]).normalized();
+  Vec2TAD nR=Vec2TAD(-dR[1],dR[0]).normalized();
   //find closest feature
-  Vec2T a[2]= {Vec2T::Zero(),_perfVelocities.col(ret._aid)*_timestep};
-  T distO=(a[1]-o[0]).dot(nO);
-  T distL=(a[1]-o[0]).dot(nL);
-  T distR=(a[1]-o[1]).dot(nR);
+  AD distO=(a[1]-o[0]).dot(nO);
+  AD distL=(a[1]-o[0]).dot(nL);
+  AD distR=(a[1]-o[1]).dot(nR);
   if(distO>=0 && distL>=0 && distR>=0) {
     //check 3 voronoi region
     if(distO<distL && distO<distR) {
       //closest feature is line segment
-      ret.pos()=(a[1]-nO*(distO+_rad))/_timestep;
-      ret.nor()=-nO;
+      ret._pos=(a[1]-nO*(distO+_rad))/_timestep;
+      ret._nor=-nO;
       ret._case+=0;
     } else if(distL<distO && distL<distR) {
       //closest feature is left border
-      ret.pos()=(a[1]-nL*(distL+_rad))/_timestep;
-      ret.nor()=-nL;
+      ret._pos=(a[1]-nL*(distL+_rad))/_timestep;
+      ret._nor=-nL;
       ret._case+=1;
     } else {
       //closest feature is right border
-      ret.pos()=(a[1]-nR*(distR+_rad))/_timestep;
-      ret.nor()=-nR;
+      ret._pos=(a[1]-nR*(distR+_rad))/_timestep;
+      ret._nor=-nR;
       ret._case+=2;
     }
   } else {
     //check 5 voronoi region
-    T t=BoundingVolumeHierarchy::closestT(a[1],o);
+    Vec2T oVal[2]= {Vec2T(o[0][0].value(),o[0][1].value()),Vec2T(o[1][0].value(),o[1][1].value())};
+    T t=BoundingVolumeHierarchy::closestT(Vec2T(a[1][0].value(),a[1][1].value()),oVal);
     if(t<=0) {
       if((a[1]-o[0]).dot(dL)>0) {
         //closest feature is left border
-        T distL=(a[1]-o[0]).dot(nL);
-        ret.pos()=(a[1]-nL*(distL+_rad))/_timestep;
-        ret.nor()=-nL;
+        AD distL=(a[1]-o[0]).dot(nL);
+        ret._pos=(a[1]-nL*(distL+_rad))/_timestep;
+        ret._nor=-nL;
         ret._case+=3;
       } else {
         //closest feature is left vertex
-        T distVL=(a[1]-o[0]).norm();
-        ret.nor()=(a[1]-o[0])/fmax(distVL,Epsilon<T>::defaultEps());
-        ret.pos()=(o[0]+ret.nor()*_rad)/_timestep;
+        AD distVL=(a[1]-o[0]).norm();
+        if(distVL.value()>Epsilon<T>::defaultEps())
+          ret._nor=(a[1]-o[0])/distVL;
+        else ret._nor=(a[1]-o[0])/Epsilon<T>::defaultEps();
+        ret._pos=(o[0]+ret._nor*_rad)/_timestep;
         ret._case+=4;
       }
     } else if(t>=1) {
       if((a[1]-o[1]).dot(dR)>0) {
         //closest feature is right border
-        T distR=(a[1]-o[1]).dot(nR);
-        ret.pos()=(a[1]-nR*(distR+_rad))/_timestep;
-        ret.nor()=-nR;
+        AD distR=(a[1]-o[1]).dot(nR);
+        ret._pos=(a[1]-nR*(distR+_rad))/_timestep;
+        ret._nor=-nR;
         ret._case+=5;
       } else {
         //closest feature is right vertex
-        T distVR=(a[1]-o[1]).norm();
-        ret.nor()=(a[1]-o[1])/fmax(distVR,Epsilon<T>::defaultEps());
-        ret.pos()=(o[1]+ret.nor()*_rad)/_timestep;
+        AD distVR=(a[1]-o[1]).norm();
+        if(distVR>Epsilon<T>::defaultEps())
+          ret._nor=(a[1]-o[1])/distVR;
+        else ret._nor=(a[1]-o[1])/Epsilon<T>::defaultEps();
+        ret._pos=(o[1]+ret._nor*_rad)/_timestep;
         ret._case+=6;
       }
     } else {
       //closets feature is line segment
-      T distO=(a[1]-o[0]).dot(nO);
-      ret.pos()=(a[1]-nO*(distO+_rad))/_timestep;
-      ret.nor()=-nO;
+      AD distO=(a[1]-o[0]).dot(nO);
+      ret._pos=(a[1]-nO*(distO+_rad))/_timestep;
+      ret._nor=-nO;
       ret._case+=7;
     }
   }
 }
+void ORCASimulator::updateLP(const VelocityObstacle& VO) const {
+
+}
+ORCASimulator::T ORCASimulator::cross2D(const Vec2TAD& a,const Vec2TAD& b) {
+  return (a[0]*b[1]-a[1]*b[0]).value();
+}
 ORCASimulator::T ORCASimulator::cross2D(const Vec2T& a,const Vec2T& b) {
   return a[0]*b[1]-a[1]*b[0];
 }
-ORCASimulator::Mat2T ORCASimulator::rot2D(T dist,T cord) {
-  T s=fmax((T)0,fmin(1-Epsilon<T>::defaultEps(),dist/cord)),c=sqrt(1-s*s);
-  Mat2T ret;
+ORCASimulator::Mat2TAD ORCASimulator::rot2D(AD dist,AD cord) {
+  AD s=dist/cord;
+  if(s.value()<=0)
+    s=AD(0,Derivative::Zero());
+  else if(s.value()>=1-Epsilon<T>::defaultEps())
+    s=AD(1-Epsilon<T>::defaultEps(),Derivative::Zero());
+  AD c=sqrt(1-s*s);
+  Mat2TAD ret;
   ret(0,0)=ret(1,1)=c;
   ret(0,1)=ret(1,0)=s;
   ret(0,1)*=-1;
--- RVO/ORCA.h
+++ RVO/ORCA.h
@@ -2,32 +2,85 @@
 #define ORCA_H
 
 #include "RVO.h"
+#include <unsupported/Eigen/AutoDiff>
 
 namespace RVO {
 class ORCASimulator : public RVOSimulator {
  public:
+  typedef Eigen::Matrix<T,8,1> Derivative;
+  typedef Eigen::AutoDiffScalar<Derivative> AD;
+  typedef Eigen::Matrix<AD,2,2> Mat2TAD;
+  typedef Eigen::Matrix<AD,2,1> Vec2TAD;
   struct VelocityObstacle {
-    Eigen::Map<Vec2T> pos() {
-      return Eigen::Map<Vec2T>(_vnab.data());
-    }
-    Eigen::Map<const Vec2T> pos() const {
-      return Eigen::Map<const Vec2T>(_vnab.data());
-    }
-    Eigen::Map<Vec2T> nor() {
-      return Eigen::Map<Vec2T>(_vnab.data()+2);
-    }
-    Eigen::Map<const Vec2T> nor() const {
-      return Eigen::Map<const Vec2T>(_vnab.data()+2);
-    }
     Vec2T proj(const Vec2T& v) const {
       return v-(v-pos()).dot(nor())*nor();
     }
     bool outside(const Vec2T& v) const {
       return (v-pos()).dot(nor())>0;
     }
-    Vec4T _vnab;    //_vab is a point on the boundary, _nab is the outward-pointing normal
-    Mat4T _DvnDpva;
-    Mat2T _DvnDpvb;
+    Vec2T pos() const {
+      return Vec2T(_pos[0].value(),_pos[1].value());
+    }
+    Vec2T nor() const {
+      return Vec2T(_nor[0].value(),_nor[1].value());
+    }
+    Mat2T DposDpa() const {
+      Mat2T ret;
+      ASSERT(_aid>=0)
+      ret.row(0)=_pos[0].derivatives().template segment<2>(0);
+      ret.row(1)=_pos[1].derivatives().template segment<2>(0);
+      return ret;
+    }
+    Mat2T DposDpb() const {
+      Mat2T ret;
+      ASSERT(_bid>=0)
+      ret.row(0)=_pos[0].derivatives().template segment<2>(2);
+      ret.row(1)=_pos[1].derivatives().template segment<2>(2);
+      return ret;
+    }
+    Mat2T DposDva() const {
+      Mat2T ret;
+      ASSERT(_aid>=0)
+      ret.row(0)=_pos[0].derivatives().template segment<2>(4);
+      ret.row(1)=_pos[1].derivatives().template segment<2>(4);
+      return ret;
+    }
+    Mat2T DposDvb() const {
+      Mat2T ret;
+      ASSERT(_bid>=0)
+      ret.row(0)=_pos[0].derivatives().template segment<2>(6);
+      ret.row(1)=_pos[1].derivatives().template segment<2>(6);
+      return ret;
+    }
+    Mat2T DnorDpa() const {
+      Mat2T ret;
+      ASSERT(_aid>=0)
+      ret.row(0)=_nor[0].derivatives().template segment<2>(0);
+      ret.row(1)=_nor[1].derivatives().template segment<2>(0);
+      return ret;
+    }
+    Mat2T DnorDpb() const {
+      Mat2T ret;
+      ASSERT(_bid>=0)
+      ret.row(0)=_nor[0].derivatives().template segment<2>(2);
+      ret.row(1)=_nor[1].derivatives().template segment<2>(2);
+      return ret;
+    }
+    Mat2T DnorDva() const {
+      Mat2T ret;
+      ASSERT(_aid>=0)
+      ret.row(0)=_nor[0].derivatives().template segment<2>(4);
+      ret.row(1)=_nor[1].derivatives().template segment<2>(4);
+      return ret;
+    }
+    Mat2T DnorDvb() const {
+      Mat2T ret;
+      ASSERT(_bid>=0)
+      ret.row(0)=_nor[0].derivatives().template segment<2>(6);
+      ret.row(1)=_nor[1].derivatives().template segment<2>(6);
+      return ret;
+    }
+    Vec2TAD _pos,_nor;    //_vab is a point on the boundary, _nab is the outward-pointing normal
     int _aid,_bid;
     int _case;
   };
@@ -37,14 +90,17 @@ class ORCASimulator : public RVOSimulator {
   virtual bool optimize(bool requireGrad,bool output) override;
   void debugVO(int aid,int bid,int testCase,T eps=1e-4f);
   void debugVO(int aid,int testCase,bool mustInside,T eps=1e-4f);
+  void debugDerivatives(const VelocityObstacle& VO);
+  void debugDerivatives(const VelocityObstacle& VO,const Vec2T o[2]);
  protected:
-  VelocityObstacle computeVelocityObstacle(int aid,int bid,bool requireGrad) const;
-  VelocityObstacle computeVelocityObstacle(int aid,Vec2T o[2],bool requireGrad) const;
-  void computeVelocityObstacle(VelocityObstacle& ret,const Vec2T& o,const Vec2T& dL,const Vec2T& dR,bool requireGrad) const;
-  void computeVelocityObstacle(VelocityObstacle& ret,const Vec2T o[2],const Vec2T& dL,const Vec2T& dR,bool requireGrad) const;
-  void updateLP(const VelocityObstacle& VO) const {}
+  VelocityObstacle computeVelocityObstacle(int aid,int bid) const;
+  VelocityObstacle computeVelocityObstacle(int aid,const Vec2T o[2]) const;
+  void computeVelocityObstacle(VelocityObstacle& ret,const Vec2TAD& o,const Vec2TAD& a,const Vec2TAD& dL,const Vec2TAD& dR) const;
+  void computeVelocityObstacle(VelocityObstacle& ret,const Vec2TAD o[2],const Vec2TAD a[2],const Vec2TAD& dL,const Vec2TAD& dR) const;
+  void updateLP(const VelocityObstacle& VO) const;
+  static T cross2D(const Vec2TAD& a,const Vec2TAD& b);
   static T cross2D(const Vec2T& a,const Vec2T& b);
-  static Mat2T rot2D(T dist,T cord);
+  static Mat2TAD rot2D(AD dist,AD cord);
 };
 }
 

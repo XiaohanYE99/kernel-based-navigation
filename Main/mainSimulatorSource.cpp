b0a385e6fc66e613ac73ab748a8cbee13d997243
pzr1988 <zherong.pan.usa@gmail.com>
2024-08-10 20:17:53 -0700

    modified all interface to use python TinyVisualizer

--- Main/mainSimulatorSource.cpp
+++ Main/mainSimulatorSource.cpp
@@ -33,6 +33,10 @@ int main(int argc,char** argv) {
     std::cout << "frame=" << frameId << " cost=" << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-beg).count() << "ms" << std::endl;
   }
   RVOVisualizer vis;
+  vis.setSourceColor(0,Eigen::Matrix<float,3,1>(1,0,0));
+  vis.setSourceColor(1,Eigen::Matrix<float,3,1>(0,1,0));
+  vis.setSourceColor(2,Eigen::Matrix<float,3,1>(0,0,1));
+  vis.setSourceColor(3,Eigen::Matrix<float,3,1>(1,0,1));
   vis.drawRVO(argc,argv,false,150,ss.getTrajectories(),rvo,[&]() {});
   return 0;
 }
